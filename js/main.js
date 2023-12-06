184cb7b55c961e5ca6261ee2b7a5e565062c3e6e
abdulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2023-12-06 18:05:03 +0500

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const elTitle1 = document.querySelector('#title1');
+const elTitle2 = document.querySelector('#title2');
+const elTitle3 = document.querySelector('#title3');
+const elTitle4 = document.querySelector('#title4');
+const elTitle5 = document.querySelector('#title5');
+const elTitle6 = document.querySelector('#title6');
+
+console.log(elTitle1.textContent = 'salom');
+console.log(elTitle2.textContent = 'olam');
+console.log(elTitle3.textContent = 'dunyo');
+console.log(elTitle4.textContent = 'mashina');
+console.log(elTitle5.textContent = 'uy');
+console.log(elTitle6.textContent = 'uy ishi');
